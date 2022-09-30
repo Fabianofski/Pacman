b304f6dbb89bf06af7162ac105f99136b4308051
MaliceL <103143489+MaliceL@users.noreply.github.com>
2022-09-30 11:03:45 +0200

    Tilemap Update

--- Assets/Scripts/Core/PlayerMovement.cs
+++ Assets/Scripts/Core/PlayerMovement.cs
@@ -38,23 +38,21 @@ namespace F4B1.Core
         public void Update()
         {
             input = moveInputAction.ReadValue<Vector2>();
-            // moveInputAction wird im Inspector definiert (schau es dir mal bei Player1 und Player2 an)
-            // Input ist ein Vector2 mit einer X und Y Achse
-            // (gespeichert in input.x oder input.y)
-            // Wenn A gedrückt wird ist X = -1, Wenn D gedrückt wird ist X = 1, sonst X = 0
-            // Wenn W gedrückt wird ist Y = 1, Wenn S gedrückt wird ist Y = -1, sonst Y = 0
-            // Wenn z.B. W und D gleichzeitig gedrückt wird ist X = 0.75 und Y = 0.75
-            // Das passiert weil man sich sonst schräg zu schnell bewegen würde wenn X und Y = 1 wären
-            // Ist ja aber irrelevant für uns da wir uns nicht schräg bewegen können
-            
-            // VVVV Kannst es hier mal ausprobieren einfach auskommentieren und WASD oder die Pfeiltasten verwenden
-            // Kommentar danach bitte löschen
-            // Debug.Log(gameobject.name + " " + input);
         }
 
         public void FixedUpdate()
         {
-            rigidbody.velocity = new Vector2(input.x * movespeed , input.y * movespeed);
+            rigidbody.velocity = input * movespeed;
+        }
+
+        public void InvertControls()
+        {
+            Invoke(nameof(ResetInvertedControls), 3f);
+        }
+
+        private void ResetInvertedControls()
+        {
+
         }
     }
 }
