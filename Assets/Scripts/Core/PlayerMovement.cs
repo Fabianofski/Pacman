f8ce720344c3aba86299e5766bea9b9aa99e40ad
MaliceL <niklas.lischke@gmx.de>
2022-10-02 17:25:13 +0200

    Items
    Inverted
    Freeze
    Fast (wrong target)
    Slow

--- Assets/Scripts/Core/PlayerMovement.cs
+++ Assets/Scripts/Core/PlayerMovement.cs
@@ -1,9 +1,3 @@
-// /**
-//  * This file is part of: Pacman
-//  * Created: 23.09.2022
-//  * Copyright (C) 2022 Fabian Friedrich
-//  * Distributed under the terms of the MIT license (cf. LICENSE.md file)
-//  **/
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityAtoms.BaseAtoms;
@@ -18,6 +12,9 @@ namespace F4B1.Core
         private Rigidbody2D rigidbody;
         private float movespeed = 5f;
         private Vector2 input;
+        private bool inverted = false;
+        private bool freeze = false;
+        private string itemmoveeffect = "normal";
 
         [SerializeField] private InputAction moveInputAction;
 
@@ -43,19 +40,62 @@ namespace F4B1.Core
 
         public void FixedUpdate()
         {
-            rigidbody.velocity = input * movespeed;
+            switch(itemmoveeffect)
+            {
+                case "normal":
+                    rigidbody.velocity = input * movespeed;
+                    break;
+                case "inverted":
+                    rigidbody.velocity = input * -1 * movespeed;
+                    break;
+                case "frozen":
+                    rigidbody.velocity = Vector2.zero;
+                    break;
+                case "fast":
+                    rigidbody.velocity = input * 10f;
+                    break;
+                case "slow":
+                    rigidbody.velocity = input * 2f;
+                    break;
+            }           
         }
 
         public void InvertControls(GameObject sender)
         {
             if (sender.name == gameObject.name) return;
-            Debug.Log("invert" + gameObject.name);
-            Invoke(nameof(ResetInvertedControls), 3f);
+            Debug.Log("invert " + gameObject.name);
+            itemmoveeffect = "inverted";
+            Invoke(nameof(ResetControls), 3f);
         }
 
-        private void ResetInvertedControls()
+        public void FreezeControls(GameObject sender)
         {
-            Debug.Log("invert reset");
+            if (sender.name == gameObject.name) return;
+            Debug.Log("freeze " + gameObject.name);
+            itemmoveeffect = "frozen";
+            Invoke(nameof(ResetControls), 3f);
+        }
+
+        public void SlowControls(GameObject sender)
+        {
+            if (sender.name == gameObject.name) return;
+            Debug.Log("slow " + gameObject.name);
+            itemmoveeffect = "slow";
+            Invoke(nameof(ResetControls), 3f);
+        }
+
+        public void FastControls(GameObject sender)
+        {
+            if (sender.name == gameObject.name) return;
+            Debug.Log("fast " + gameObject.name);
+            itemmoveeffect = "fast";
+            Invoke(nameof(ResetControls), 3f);
+        }
+
+        private void ResetControls()
+        {
+            Debug.Log("controls reset");
+            itemmoveeffect = "normal";
         }
     }
 }
