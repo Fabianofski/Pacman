9b70266f0c8dd25ff6fdbf1a18ced77475047b9c
Fabian Friedrich <friedrich.fabian@gmx.net>
2022-10-16 12:34:36 +0200

    Added Ready Screen on Game Start

---
+++ Assets/Scripts/UI/ReadyScreen.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace F4B1.UI
+{
+    public class ReadyScreen : MonoBehaviour
+    {
+
+        [SerializeField] private float readyTime;
+        
+        private void Awake()
+        {
+            StartCoroutine(StartGame());
+            StartCoroutine(AnimateText());
+        }
+
+        private IEnumerator StartGame()
+        {
+            Time.timeScale = 0;
+            yield return new WaitForSecondsRealtime(readyTime);
+            Time.timeScale = 1;
+            Destroy(gameObject);
+        }
+
+        private IEnumerator AnimateText()
+        {
+            gameObject.transform.localScale = Vector3.zero;
+            LeanTween.scale(gameObject, Vector3.one, readyTime / 4).setIgnoreTimeScale(true);;
+            
+            LeanTween.value(gameObject, 0, 1, readyTime / 2).setOnUpdate(val =>
+            {
+                var text = GetComponentInChildren<TextMeshProUGUI>();
+                var col = text.color;
+                col.a = val;
+                text.color = col;
+            }).setIgnoreTimeScale(true);
+            
+            yield return new WaitForSecondsRealtime((readyTime/4) * 3);
+            
+            LeanTween.value(gameObject, 1, 0, readyTime / 4).setOnUpdate((val) =>
+            {
+                var text = GetComponentInChildren<TextMeshProUGUI>();
+                var col = text.color;
+                col.a = val;
+                text.color = col;
+            }).setIgnoreTimeScale(true);;
+        }
+    }
+}
